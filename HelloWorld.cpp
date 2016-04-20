e0f3ffb87566bc1bf53e14f30cc47bd344a59a33
Francisco Gómez <espectalll123@kydara.com>
2016-04-20 17:19:56 +0200

    Made better

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,25 +1,35 @@
-#include <stdio.h>
+#include <iostream>
 #include <afxinet.h>
+using namespace std;
 
-int main() {
-	printf("Establishing connection...\n");
+int main(int argc, char* argv[]) {
+	LPCTSTR ip = "10.0.2.2";
+	INTERNET_PORT port = 80;
+	LPCTSTR path = "/";
 
-	LPCTSTR ip= "10.0.2.2";
-	INTERNET_PORT port = 8080;
-	LPCTSTR path="/";
+	if (argv[1] != NULL) {
+		ip = argv[1];
+		if (argv[2] != NULL) {
+			port = (unsigned short)(LPWSTR) argv[2];
+		}
+	}
 
-	CInternetSession session("My Session");
+	cout << "Establishing connection to "
+		<< ip << ":" << port << "..." << endl;
+
+	CInternetSession session("HelloWorld");
 	CHttpConnection* connection = session.GetHttpConnection(
 		ip, port);
-	CHttpFile* res = connection->OpenRequest(
-		( LPCTSTR ) "GET", path);
+	CHttpFile* res = connection->OpenRequest("GET", path);
 	CHAR buff[1024];
 	if (res->SendRequest()) {
-		res->Read(buff, 1024);
-		printf("%1023s\n", buff);
+		while(res->Read(buff, 1024)) {
+			cout << buff;
+		}
+		cout << endl;
 	}
 	else {
-		printf("Sorry, unable to connect!");
+		cout << "Sorry, unable to connect!" << endl;
 		delete res;
 		session.Close();
 		return 1;
